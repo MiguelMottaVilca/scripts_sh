697f57886832ed065a945cd04fde0f1098f0cf45
MiguelMottaVilca <miguel.motta@ucsp.edu.pe>
2021-11-02 15:11:23 -0500

    pruebas 02

--- myscript2.sh
+++ myscript2.sh
@@ -1,5 +1,34 @@
 #! /bin/bash
 
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+trap ctrl_c INT
+
+function ctrl_c(){
+  echo -e "\n${redColour}[!] Saliendo .... \n"${endColour}
+  exit 1
+}
+
+function helpPanel(){
+  echo -e "\n${redColour} Estas en el menu de ayuda ${endColour}"
+}
+
+while getopts "e:h:" arg;
+do
+  case $arg in
+    e) exploration_mode=$OPTARG;;
+    h) helpPanel;;
+  esac
+done
+
 echo "Introduce tu nombre : "
 read nombre
 echo "Tu nombre es ${nombre}"
